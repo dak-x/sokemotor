c92c02c2f2ea3434e37186a37bc6288f1aaff192
Daksh Chauhan <dak-x@outlook.com>
2021-07-30 11:34:39 +0530

    Make sokemotor wait for es instance to start up.
    Signed-off-by: Daksh Chauhan <dak-x@outlook.com>

--- main.go
+++ main.go
@@ -2,17 +2,52 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/gin-gonic/gin"
 )
 
+func getEsClint() *elasticsearch.Client {
+
+	esCfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://elastic:9200/",
+		},
+	}
+
+	es, err := elasticsearch.NewClient(esCfg)
+
+	if err != nil {
+		log.Fatalf("Cannot create Client: %v", err)
+	}
+
+	res, err := es.Info()
+
+	var numRetry int = 0
+
+	for err != nil {
+		time.Sleep(3 * time.Second)
+		numRetry++
+		if numRetry == 5 {
+			break
+		}
+		res, err = es.Info()
+	}
+
+	if err != nil {
+		log.Fatalf("Elastic is not running %s", err)
+	}
+
+	log.Printf("ELASTIC IS UP: %v", res)
+	log.Printf("STARTING SOKEMOTOR SERVICE")
+
+	return es
+}
+
 func main() {
 
-	_, err := elasticsearch.NewDefaultClient()
-	if err != nil {
-		log.Fatal("Elastic Instance Not Running Exiting...")
-	}
+	_ = getEsClint()
 
 	app := gin.Default()
 	app.GET("/search/:query", searchHandler)
