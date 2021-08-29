e8cb2b1aed690c30d01b4b7ed0014c46039c1f12
Navdeep Singh <navdeepsuneha@gmail.com>
2021-08-29 22:56:05 +0530

    integrated backend apis with the front-end ui

--- soke-vise/src/components/Card/index.jsx
+++ soke-vise/src/components/Card/index.jsx
@@ -4,8 +4,8 @@ export default function Card({title,innerText,date,link}) {
 	return (
 		<div className="card">
 		<div className="title"	>{title}</div>
-		<div className="inner-text">
-			{innerText}
+		<div className="inner-text" div dangerouslySetInnerHTML={{ __html: innerText }}>
+			
 		</div>
 		<div className="flex-container">
 		<div className="last-visited"> {date}</div>
--- soke-vise/src/components/MainBody/index.jsx
+++ soke-vise/src/components/MainBody/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import "./mainBody.css"
 import { useState } from 'react'
-
+import { baseUrl } from '../../baseUrl'
 import Card from '../Card'
-
 
 
 
@@ -11,71 +10,88 @@ import Card from '../Card'
 
 export default function MainBody() {
 	const [searchName, setSearchName] = useState("")
-
 	const [isSelected, setIsSelected] = useState(false)
 	const [dropDownItems, setDropDownItems] = useState(["hello", "hi"])
 	const [selectedDropDownItems, setSelectedDropDownItems] = useState([])
 
-	const [renderValues,setRenderValues] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [renderValues, setRenderValues] = useState([])
+	const render_cards = (data) => {
 
-	function search(){
-		var objLis=[
-			{
-			
-				"lastaccessed":"date-time",
-	"title":`${searchName}`,
-	"url":"google.com",
-	"dom":"hkuhlkuhkg",
-		
-		},
-			{
-			
-				"lastaccessed":"date-time",
-	"title":"something",
-	"url":"google.com",
-	"dom":"hkuhlkuhkg",
-		
-		},
-			{
-			
-				"lastaccessed":"date-time",
-	"title":"something",
-	"url":"google.com",
-	"dom":"hkuhlkuhkg",
-		
-		},
-		]
-		setRenderValues(objLis)
+		var lis = data.map((elem) => {
+
+
+			var title = "Html Source"
+			var url = elem._source.url
+			var lastaccessed = elem._source.lastaccessed
+			var dom = elem.highlight.dom[0]
+
+			return {
+				title, url, lastaccessed, dom
+
+			}
+
+
+		})
+		if(lis.length===0){
+			lis=[{title:"No data found",url:"",lastaccessed:"",dom:""}]
+		}
+		return lis
+
+
 	}
-	
-	function makeOptions(lis) {
+
+	function search() {
+
+
+
+		setIsLoading(true)
+
+		fetch(`${baseUrl}/search/${searchName}`)
+			.then(response => response.json())
+			.then(data => {
+				setIsLoading(false)
+				setRenderValues(render_cards(data))
+
+			}
+
+			);
+
+	}
+
+	const handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			search()
+		}
+	}
+	const makeOptions = (lis) => {
 		console.log("hello there", lis)
-		var obj = lis.map((x,index) => <button className="drop-down-item"
+		var obj = lis.map((x, index) => <button className="drop-down-item"
 			key={x}
-			
-			onClick={()=>setSelectedDropDownItems(a=>{
-			
-				console.log(a,"this is a")
+
+			onClick={() => setSelectedDropDownItems(a => {
+
+				console.log(a, "this is a")
 				var lis = a
 				lis.push(x)
-				setDropDownItems(b=>{
+				setDropDownItems(b => {
 
 					var new_lis = []
-					b.forEach((val,ind) =>ind!=index&&new_lis.push(val))
+					b.forEach((val, ind) => ind != index && new_lis.push(val))
 					return new_lis
 				})
-			return 	lis
-			
+				return lis
+
 			})}
-			>{x}
-			
+		>{x}
+
 		</button>
 		)
 		console.log(obj)
 		return <div className="drop-downs"
 
 
-			onPointerLeave={()=> setIsSelected(false)}
+			onPointerLeave={() => setIsSelected(false)}
 		>{obj}</div>
 
 	}
@@ -86,62 +102,71 @@ export default function MainBody() {
 				<input type="text"
 					className="search-bar"
 					placeholder="Seach any item"
+					onKeyPress={handleKeyDown}
 					value={searchName}
 					onChange={({ target }) => setSearchName(target.value)}
 
 
 				></input>
 				<div className="select-box"
-				onClick={()=>setIsSelected(x=>!x)}
+					onClick={() => setIsSelected(x => !x)}
 				>
-					
+
 					<button
 						className="select"
 					>
-						
-						
-						{selectedDropDownItems.length<2?`${selectedDropDownItems.length} item selected`:`${selectedDropDownItems.length} items selected`}
-						
-						
-						
-						</button>
+
+
+						{selectedDropDownItems.length < 2 ? `${selectedDropDownItems.length} item selected` : `${selectedDropDownItems.length} items selected`}
+
+
+
+					</button>
 					{isSelected && makeOptions(dropDownItems)}
 				</div>
-				<button className="search-button" onClick={()=>search()}>Search</button>
+				<button className="search-button" onClick={() => search()}>Search</button>
 			</div>
-			<div style={{display:"flex"}}>
-			{selectedDropDownItems.map((x,index) => <div className="bottom-selected-items"
-			>{x}
-			<button className="cross-icon"
+			<div className="selected-container">
+				{selectedDropDownItems.map((x, index) => <div className="bottom-selected-items"
+				>{x}
+					<button className="cross-icon"
 
-			onClick={()=>setDropDownItems(a=>{
-			
-				console.log(a,"this is a")
-				var lis = a
-				lis.push(x)
-				setSelectedDropDownItems(b=>{
+						onClick={() => setDropDownItems(a => {
 
-					var new_lis = []
-					b.forEach((val,ind) =>ind!=index&&new_lis.push(val))
-					return new_lis
-				})
-			return 	lis
-			
-			})}
-			>x</button></div>)}
+							console.log(a, "this is a")
+							var lis = a
+							lis.push(x)
+							setSelectedDropDownItems(b => {
+
+								var new_lis = []
+								b.forEach((val, ind) => ind != index && new_lis.push(val))
+								return new_lis
+							})
+							return lis
+
+						})}
+					>x</button></div>)}
 			</div>
-<div className="card-container">
-			{renderValues.map(x =>
+			<div className="card-container">
+
+				{isLoading ? <div className="loading">Loading...</div> :
+					renderValues.map(x =>
+
+						<Card
+							title={x.title}
+							innerText={x.dom}
+							date={x.lastaccessed}
+							link={x.url}
+
+						/>
+					)
+
+
+
+				}
+
 				
-<Card
-title={x.title}
-innerText={x.dom}
-date={x.lastaccessed}
-link={x.url}
-
-/>
-				)}
-				</div>
+			</div>
 		</div>
 	)
 }
